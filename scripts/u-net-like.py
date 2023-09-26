656acce8cee3efc931b5c51b717a34f2d21937ab
Lucas Brito <lucas.brito@agi.com.br>
2023-09-26 11:19:52 -0300

    feat: add u-net-like code as script

---
+++ scripts/u-net-like.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from scripts.bpca_pooling import BPCAPooling
+
+
+def get_unetlike_model(img_size, num_classes):
+    inputs = keras.Input(shape=img_size + (3,))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = keras.layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = keras.layers.Activation("relu")(x)
+        x = keras.layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = keras.layers.BatchNormalization()(x)
+
+        x = keras.layers.Activation("relu")(x)
+        x = keras.layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = keras.layers.BatchNormalization()(x)
+
+        # x = keras.layers.MaxPooling2D(2, strides=2, padding="same")(x)
+        # print(filters)
+        x = BPCAPooling(pool_size=2, stride=2,
+                        expected_shape=get_layers_number(filters))(x)
+        # print(filters, x.shape)
+
+        # Project residual
+        residual = keras.layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = keras.layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = keras.layers.Activation("relu")(x)
+        x = keras.layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = keras.layers.BatchNormalization()(x)
+
+        x = keras.layers.Activation("relu")(x)
+        x = keras.layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = keras.layers.BatchNormalization()(x)
+
+        x = keras.layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = keras.layers.UpSampling2D(2)(previous_block_activation)
+        residual = keras.layers.Conv2D(filters, 1, padding="same")(residual)
+        x = keras.layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    # Add a per-pixel classification layer
+    outputs = keras.layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(
+        x
+    )
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
+
+
+model = get_unetlike_model(img_size=(HEIGHT, WIDTH), num_classes=NUM_CLASSES)
+model.summary()
