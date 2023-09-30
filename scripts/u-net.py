22469533bba8bf76b18c1cbeb8e95bbde050086a
Lucas Brito <lucasbsilva29@gmail.com>
2023-09-29 22:09:43 -0300

    fix: name and identation

--- scripts/u-net.py
+++ scripts/u-net.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -34,42 +33,32 @@ def get_layers_number(number):
 
 def get_unet_model(img_size, num_classes):
     inputs = layers.Input(shape=img_size + (3,))
-    # Test the following lines (until the loop) to see if the model works
     x = keras.layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
-    x = keras.layers.BatchNormalization()(inputs)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.Activation("relu")(x)
 
-    ### [First half of the network: downsampling inputs] ###
-    concatenatenating_layers = []
+    concatenating_layers = []
     for filters in [64, 128, 256, 512]:
         x = layers.Conv2D(filters, activation='relu', kernel_size=3)(x)
         x = layers.Conv2D(filters, activation='relu', kernel_size=3)(x)
-        concatenatenating_layers.append(x)
-        x = layers.MaxPool2D(pool_size=(
-            2, 2), strides=(2, 2), padding='valid')(x)
-        # x = BPCAPooling(pool_size=2, stride=2, expected_shape=get_layers_number(filters))(x)
+        concatenating_layers.append(x)
+        x = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
 
     x = layers.Conv2D(1024, activation='relu', kernel_size=3)(x)
-    x = layers.Conv2D(1024, activation='relu',
-                      kernel_size=3, padding='valid')(x)
+    x = layers.Conv2D(1024, activation='relu', kernel_size=3, padding='valid')(x)
 
-    ### [Second half of the network: upsampling inputs] ###
     for filters in [512, 256, 128]:
-        transp = layers.Conv2DTranspose(
-            filters, kernel_size=2, strides=(2, 2), activation='relu')(x)
-        conc_conv = concatenatenating_layers.pop()
-        crop = layers.Cropping2D(
-            cropping=define_crop_values(filters))(conc_conv)
+        transp = layers.Conv2DTranspose(filters, kernel_size=2, strides=(2, 2), activation='relu')(x)
+        conc_conv = concatenating_layers.pop()
+        crop = layers.Cropping2D(cropping=define_crop_values(filters))(conc_conv)
         conc = layers.concatenate([transp, crop], axis=-1)
 
         x = layers.Conv2D(filters, activation='relu', kernel_size=3)(conc)
         x = layers.Conv2D(filters, activation='relu', kernel_size=3)(x)
 
-    outputs = layers.Conv2D(num_classes, kernel_size=3,
-                            activation='softmax', padding="same")(x)
+    outputs = layers.Conv2D(num_classes, kernel_size=3, activation='softmax', padding="same")(x)
     model = keras.Model(inputs=inputs, outputs=outputs)
     return model
-
 
 HEIGHT, WIDTH = 256, 256
 NUM_CLASSES = 3  # background, foreground, boundary
